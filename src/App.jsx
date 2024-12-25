042b39cd29857bc2cce80fd91e9cf70d1f76c611
Stepan Suvorov <stevermeister@gmail.com>
2024-12-25 21:41:16 +0100

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Wishlist from './pages/Wishlist';
 import Login from './pages/Login';
